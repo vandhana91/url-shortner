6bd35c4c769e89f612ea5a596d383b8ca88b051f
vandhana91 <85727736+vandhana91@users.noreply.github.com>
2022-09-07 20:03:36 +0530

    Create BoostLinksCTA.js

---
+++ src/components/BoostLinksCTA.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BoostLinksCTA = () => {
+  return (
+    <div className={'boost__links flex flex__column flex__jc-c flex__ai-c'}>
+      <h1>Boost your links today</h1>
+      <a href='/'>Get Started</a>
+    </div>
+  );
+};
+
+export default BoostLinksCTA;
