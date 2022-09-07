eeb50e2a67d7e65d76a5471cc7f266d7f8cfde74
vandhana91 <85727736+vandhana91@users.noreply.github.com>
2022-09-07 20:10:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import BoostLinksCTA from './components/BoostLinksCTA';
+import FormStats from './components/FormStats';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <FormStats />
+      <BoostLinksCTA />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
